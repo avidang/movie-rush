d9007a9f31ae67cc8254ecf887a9b1e143cbfc7c
avidang01~ <avidan627@gmail.com>
2026-02-15 10:03:30 +0200

    Add parameters to getMovieDetails API call to include video data

--- src/api/movies/details.ts
+++ src/api/movies/details.ts
@@ -5,6 +5,10 @@ import { movieDetailSchema } from '@/schemas/movie-detail';
 const API_PATH = '/movie';
 
 export const getMovieDetails = async (movieId: number) => {
-  const response = await apiClient.get(`${API_PATH}/${movieId}`);
+  const response = await apiClient.get(`${API_PATH}/${movieId}`, {
+    params: {
+      append_to_response: 'videos',
+    },
+  });
   return movieDetailSchema.parse(response.data);
 };
